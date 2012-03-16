3c80bad3fa45fba28823cf35eb15aa7aa9cd9352
Si Fraser <simon@siniatech.com>
2012-03-16 18:20:01 +0000

    begin prettying-up the button bar

--- dokz/src/main/java/com/siniatech/dokz/DokzButtonBar.java
+++ dokz/src/main/java/com/siniatech/dokz/DokzButtonBar.java
@@ -1,6 +1,11 @@
 package com.siniatech.dokz;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.LinearGradientPaint;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
@@ -19,15 +24,19 @@ public class DokzButtonBar extends JComponent {
         JButton close = new JButton( "Close" );
 
         JPanel buttonPanel = new JPanel();
+        buttonPanel.setOpaque( false );
         buttonPanel.add( min );
         buttonPanel.add( max );
         buttonPanel.add( close );
 
         JPanel buttonBar = new JPanel( new BorderLayout() );
+        buttonBar.setOpaque( false );
         buttonBar.add( buttonPanel, BorderLayout.EAST );
 
         if ( title != null && title.length() > 0 ) {
-            buttonBar.add( new JLabel( title ), BorderLayout.WEST );
+            JLabel label = new JLabel( title );
+            label.setOpaque( false );
+            buttonBar.add( label, BorderLayout.WEST );
         }
 
         min.addActionListener( new AbstractAction() {
@@ -52,4 +61,14 @@ public class DokzButtonBar extends JComponent {
         setLayout( new BorderLayout() );
         add( buttonBar, BorderLayout.CENTER );
     }
+
+    @Override
+    protected void paintComponent( Graphics g ) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setPaint( new LinearGradientPaint( new Point( 0, 0 ), new Point( 0, 16 ), new float[] { 0.2f, 0.8f }, new Color[] { Color.lightGray, new Color( 228, 228, 228 ) } ) );
+        g2.fillRect( 8, 0, getWidth() - 16, 16 );
+        g2.fillArc( 0, 0, 16, 32, 90, 180 );
+        g2.fillArc( getWidth() - 16, 0, 16, 32, 180, 270 );
+    }
+
 }
