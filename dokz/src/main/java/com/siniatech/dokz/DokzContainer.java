8250a8134404a763c229d72144d866ac64f04c3e
Si Fraser <simon@siniatech.com>
2012-04-11 19:26:41 +0100

    extract resize listeners to container

--- dokz/src/main/java/com/siniatech/dokz/DokzContainer.java
+++ dokz/src/main/java/com/siniatech/dokz/DokzContainer.java
@@ -1,17 +1,115 @@
 package com.siniatech.dokz;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.List;
 
 import javax.swing.JPanel;
 
 import com.siniatech.dokz.context.DokzContext;
+import com.siniatech.dokz.layout.DokzLayoutManager;
 
 public class DokzContainer extends JPanel {
 
     private final DokzContext dokzContext;
 
-    public DokzContainer( DokzContext dokzContext ) {
+    private Point resizeStartPoint = null;
+    private int p1w;
+    private int p2w;
+    private int p2x;
+
+    boolean isResizeStarted() {
+        return resizeStartPoint != null;
+    }
+
+    void endResize() {
+        resizeStartPoint = null;
+    }
+
+    void startResize( Point point ) {
+        resizeStartPoint = point;
+        List<DokzPanel> keySet = dokzContext.getPanels();
+        DokzPanel p1 = keySet.get( 0 );
+        DokzPanel p2 = keySet.get( 1 );
+        p1w = p1.getWidth();
+        p2w = p2.getWidth();
+        p2x = p2.getX();
+    }
+    
+    JPanel getPanelAt( Point point ) {
+        for ( DokzPanel panel : dokzContext.getPanels() ) {
+            if ( panel.getBounds().contains( point ) ) {
+                return panel;
+            }
+        }
+        return null;
+    }
+    
+    public DokzContainer( final DokzContext dokzContext ) {
         this.dokzContext = dokzContext;
+        
+        setLayout( new DokzLayoutManager( dokzContext ) );
+        setBackground( Color.lightGray );
+        addMouseMotionListener( new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved( MouseEvent e ) {
+                Cursor ewResizeCursor = Cursor.getPredefinedCursor( Cursor.E_RESIZE_CURSOR );
+                if ( getPanelAt( e.getPoint() ) == null && getCursor() != ewResizeCursor ) {
+                    setCursor( ewResizeCursor );
+                } else if ( getCursor() != Cursor.getDefaultCursor() ) {
+                    setCursor( Cursor.getDefaultCursor() );
+                }
+            }
+
+            @Override
+            public void mouseDragged( MouseEvent e ) {
+                if ( !isResizeStarted() ) {
+                    return;
+                }
+
+                int xMove = e.getPoint().x - resizeStartPoint.x;
+                List<DokzPanel> keySet = dokzContext.getPanels();
+                DokzPanel p1 = keySet.get( 0 );
+                DokzPanel p2 = keySet.get( 1 );
+                Rectangle p1b = p1.getBounds();
+                Rectangle p2b = p2.getBounds();
+                p1.setBounds( p1b.x, p1b.y, p1w + xMove, p1b.height );
+                p2.setBounds( p2x + xMove, p2b.y, p2w - xMove, p2b.height );
+                invalidate();
+                p1.validate();
+                p2.validate();
+            }
+        } );
+        addMouseListener( new MouseAdapter() {
+            @Override
+            public void mouseExited( MouseEvent e ) {
+                setCursor( Cursor.getDefaultCursor() );
+            }
+
+            @Override
+            public void mousePressed( MouseEvent e ) {
+                if ( getPanelAt( e.getPoint() ) == null ) {
+                    startResize( e.getPoint() );
+                }
+            }
+
+            @Override
+            public void mouseReleased( MouseEvent e ) {
+                if ( isResizeStarted() ) {
+                    endResize();
+                } else if ( e.getClickCount() > 1 ) {
+                    System.out.println( "POP" );
+                    // these listeners probably need be in the container
+
+                }
+            }
+        } );
     }
 
     public Component add( DokzPanel comp ) {
@@ -27,5 +125,8 @@ public class DokzContainer extends JPanel {
     public DokzContext getDokzContext() {
         return dokzContext;
     }
+    
+    
+    
 
 }
--- dokz/src/main/java/com/siniatech/dokz/DokzManager.java
+++ dokz/src/main/java/com/siniatech/dokz/DokzManager.java
@@ -1,21 +1,10 @@
 package com.siniatech.dokz;
 
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.util.List;
-
 import javax.swing.JComponent;
-import javax.swing.JPanel;
 
 import com.siniatech.dokz.api.IDokzContext;
 import com.siniatech.dokz.context.DokzContext;
 import com.siniatech.dokz.context.DokzPanelContext;
-import com.siniatech.dokz.layout.DokzLayoutManager;
 import com.siniatech.siniautils.fn.IResponse0;
 import com.siniatech.siniautils.swing.IAmJComponent;
 
@@ -27,96 +16,7 @@ public class DokzManager implements IAmJComponent {
         dokzContext = new DokzContext();
         final DokzContainer mainContainer = new DokzContainer( dokzContext );
         dokzContext.setMainContainer( mainContainer );
-        mainContainer.setLayout( new DokzLayoutManager( dokzContext ) );
-        mainContainer.setBackground( Color.lightGray );
-        mainContainer.addMouseMotionListener( new MouseMotionAdapter() {
-            @Override
-            public void mouseMoved( MouseEvent e ) {
-                Cursor ewResizeCursor = Cursor.getPredefinedCursor( Cursor.E_RESIZE_CURSOR );
-                if ( getPanelAt( e.getPoint() ) == null && mainContainer.getCursor() != ewResizeCursor ) {
-                    mainContainer.setCursor( ewResizeCursor );
-                } else if ( mainContainer.getCursor() != Cursor.getDefaultCursor() ) {
-                    mainContainer.setCursor( Cursor.getDefaultCursor() );
-                }
-            }
 
-            @Override
-            public void mouseDragged( MouseEvent e ) {
-                if ( !isResizeStarted() ) {
-                    return;
-                }
-
-                int xMove = e.getPoint().x - resizeStartPoint.x;
-                List<DokzPanel> keySet = dokzContext.getPanels();
-                DokzPanel p1 = keySet.get( 0 );
-                DokzPanel p2 = keySet.get( 1 );
-                Rectangle p1b = p1.getBounds();
-                Rectangle p2b = p2.getBounds();
-                p1.setBounds( p1b.x, p1b.y, p1w + xMove, p1b.height );
-                p2.setBounds( p2x + xMove, p2b.y, p2w - xMove, p2b.height );
-                mainContainer.invalidate();
-                p1.validate();
-                p2.validate();
-            }
-        } );
-        mainContainer.addMouseListener( new MouseAdapter() {
-            @Override
-            public void mouseExited( MouseEvent e ) {
-                mainContainer.setCursor( Cursor.getDefaultCursor() );
-            }
-
-            @Override
-            public void mousePressed( MouseEvent e ) {
-                if ( getPanelAt( e.getPoint() ) == null ) {
-                    startResize( e.getPoint() );
-                }
-            }
-
-            @Override
-            public void mouseReleased( MouseEvent e ) {
-                if ( isResizeStarted() ) {
-                    endResize();
-                } else if ( e.getClickCount() > 1 ) {
-                    System.out.println( "POP" );
-                    // these listeners probably need be in the container
-
-                }
-            }
-        } );
-    }
-
-    private Point resizeStartPoint = null;
-    private int p1w;
-    private int p2w;
-    private int p2x;
-
-    boolean isResizeStarted() {
-        return resizeStartPoint != null;
-    }
-
-    void endResize() {
-        resizeStartPoint = null;
-    }
-
-    void startResize( Point point ) {
-        resizeStartPoint = point;
-        List<DokzPanel> keySet = dokzContext.getPanels();
-        DokzPanel p1 = keySet.get( 0 );
-        DokzPanel p2 = keySet.get( 1 );
-        p1w = p1.getWidth();
-        p2w = p2.getWidth();
-        p2x = p2.getX();
-    }
-
-    /////////
-
-    JPanel getPanelAt( Point point ) {
-        for ( DokzPanel panel : dokzContext.getPanels() ) {
-            if ( panel.getBounds().contains( point ) ) {
-                return panel;
-            }
-        }
-        return null;
     }
 
     JComponent createButtonBarFor( final DokzPanel dokzPanel, String title ) {
