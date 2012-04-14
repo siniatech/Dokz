6bdafbbb799c86a8f3dc3bd6c3ad4b62e211a08f
Si Fraser <simon@siniatech.com>
2012-04-14 17:36:00 +0100

    tryToFillGapByExtendingOneComponent in removing layouter - to be tested

--- dokz/src/main/java/com/siniatech/dokz/layout/RemovingLayouter.java
+++ dokz/src/main/java/com/siniatech/dokz/layout/RemovingLayouter.java
@@ -1,7 +1,10 @@
 package com.siniatech.dokz.layout;
 
+import static com.siniatech.siniautils.swing.BoundsHelper.*;
+
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.geom.Area;
 import java.util.Collection;
@@ -16,6 +19,80 @@ public class RemovingLayouter extends AbstractLayouter {
     @Override
     public <T extends Component> void doLayout( Collection<T> components, Dimension size, int hGap, int vGap ) {
         Rectangle gap = findGap( components, size, hGap, vGap );
+        boolean isFilled = tryToFillGapByExtendingOneComponent( components, gap, size, hGap, vGap );
+        if ( !isFilled ) {
+            // try something else
+        }
+    }
+
+    @DefaultForTesting
+    boolean tryToFillGapByExtendingOneComponent( Collection<? extends Component> components, Rectangle gap, Dimension size, int hGap, int vGap ) {
+        return tryToFillGapByExtendingOneComponentW( components, gap, hGap ) || //
+            tryToFillGapByExtendingOneComponentE( components, gap, size, hGap ) || //
+            tryToFillGapByExtendingOneComponentN( components, gap, vGap ) || //
+            tryToFillGapByExtendingOneComponentS( components, gap, size, vGap );
+    }
+
+    private boolean tryToFillGapByExtendingOneComponentN( Collection<? extends Component> components, Rectangle gap, int vGap ) {
+        int x = gap.x;
+        int y = gap.y - vGap - 1;
+        if ( y >= 0 ) {
+            Component compToTop = getComponentContaining( components, new Point( x, y ) );
+            if ( compToTop != null && alignsVertically( compToTop.getBounds(), gap ) ) {
+                Rectangle bounds = compToTop.getBounds();
+                int height = bounds.height + gap.height + vGap;
+                compToTop.setBounds( bounds.x, bounds.y, bounds.width, height );
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryToFillGapByExtendingOneComponentS( Collection<? extends Component> components, Rectangle gap, Dimension size, int vGap ) {
+        int x = gap.x;
+        int y = gap.y + gap.height + vGap;
+        if ( x <= size.height ) {
+            Component compToBottom = getComponentContaining( components, new Point( x, y ) );
+            if ( compToBottom != null && alignsVertically( compToBottom.getBounds(), gap ) ) {
+                Rectangle bounds = compToBottom.getBounds();
+                int height = bounds.height + gap.height + vGap;
+                int newY = bounds.y - gap.height - vGap;
+                compToBottom.setBounds( bounds.x, newY, bounds.width, height );
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryToFillGapByExtendingOneComponentW( Collection<? extends Component> components, Rectangle gap, int hGap ) {
+        int x = gap.x - hGap - 1;
+        int y = gap.y;
+        if ( x >= 0 ) {
+            Component compToLeft = getComponentContaining( components, new Point( x, y ) );
+            if ( compToLeft != null && alignsHorizontally( compToLeft.getBounds(), gap ) ) {
+                Rectangle bounds = compToLeft.getBounds();
+                int width = bounds.width + gap.width + hGap;
+                compToLeft.setBounds( bounds.x, bounds.y, width, bounds.height );
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean tryToFillGapByExtendingOneComponentE( Collection<? extends Component> components, Rectangle gap, Dimension size, int hGap ) {
+        int x = gap.x + gap.width + hGap;
+        int y = gap.y;
+        if ( x <= size.width ) {
+            Component compToRight = getComponentContaining( components, new Point( x, y ) );
+            if ( compToRight != null && alignsHorizontally( compToRight.getBounds(), gap ) ) {
+                Rectangle bounds = compToRight.getBounds();
+                int width = bounds.width + gap.width + hGap;
+                int newX = bounds.x - gap.width - hGap;
+                compToRight.setBounds( newX, bounds.y, width, bounds.height );
+                return true;
+            }
+        }
+        return false;
     }
 
     @DefaultForTesting
