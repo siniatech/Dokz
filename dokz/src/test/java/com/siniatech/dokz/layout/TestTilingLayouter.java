f04221159a22100ecf130b1aeeb7411739fd4a22
si <si@sifraser.co.uk>
2012-05-29 13:13:14 +0100

    add license/headers

--- dokz/src/test/java/com/siniatech/dokz/layout/TestTilingLayouter.java
+++ dokz/src/test/java/com/siniatech/dokz/layout/TestTilingLayouter.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Dokz
+ * Copyright (c) 2011-2 Siniatech Ltd  http://www.siniatech.com/products/dokz
+ *
+ * All rights reserved. This project and the accompanying materials are made 
+ * available under the terms of the MIT License which can be found in the root  
+ * of the project, and at http://www.opensource.org/licenses/mit-license.php
+ *
+ ******************************************************************************/
 package com.siniatech.dokz.layout;
 
 import static junit.framework.Assert.*;
